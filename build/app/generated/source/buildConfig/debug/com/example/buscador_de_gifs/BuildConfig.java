92dbd0f7067a054e30bfec8ce9732cc0a6c29e5f
Leticia <leticiamonteiro25@hotmail.com>
2021-04-08 23:19:50 -0300

    Primeiro commit

---
+++ build/app/generated/source/buildConfig/debug/com/example/buscador_de_gifs/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.buscador_de_gifs;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.buscador_de_gifs";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
